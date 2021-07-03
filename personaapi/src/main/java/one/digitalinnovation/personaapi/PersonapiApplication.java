9fcbc1996ddd8e973aed6c788363af846953c8e4
Sérgio Galdino <rudnaevois@gmail.com>
2021-07-02 23:40:42 -0300

    first commit 2

---
+++ personaapi/src/main/java/one/digitalinnovation/personaapi/PersonapiApplication.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.personaapi;
+
+import one.digitalinnovation.personaapi.controller.PersonController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+	
+
+}
---
+++ personaapi/src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personaapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "API TEST!";
+    }
+}
